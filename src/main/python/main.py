feca97b2917bd3ef151478a8aed54f16db3eb05c
bhavesh-jadav <bhaveshhjadav@gmail.com>
2019-01-23 19:24:22 +0530

    Removed icons

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -3,10 +3,12 @@ from PowerBIThemeGeneratorGUI import PowerBIThemeGeneratorWindow
 
 import sys
 
+
 class AppContext(ApplicationContext):           # 1. Subclass ApplicationContext
     def run(self):                              # 2. Implement run()
         window = PowerBIThemeGeneratorWindow()
         return self.app.exec_()                 # 3. End run() with this line
+
 
 if __name__ == '__main__':
     appctxt = AppContext()                      # 4. Instantiate the subclass
