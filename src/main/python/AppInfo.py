4ff44afb3616092f5217f00405566e37ee7901a6
bhavesh-jadav <bhaveshhjadav@gmail.com>
2019-03-01 14:29:10 +0530

    Updated AppInfo.py

--- src/main/python/AppInfo.py
+++ src/main/python/AppInfo.py
@@ -1,7 +1,7 @@
 
 
 Name = 'Power BI Theme Generator'
-Version = 'v0.3.0'
+Version = 'v0.4.0'
 Author = 'Bhavesh Jadav'
 GitHubRepoURL = 'https://github.com/bhavesh-jadav/Power-BI-Theme-Generator'
 GitHubRepoIssuesURL = GitHubRepoURL + '/issues'
